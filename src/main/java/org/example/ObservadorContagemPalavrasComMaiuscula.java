947b1dc9c3853304a3a27ac75cc8278dd9881d41
DaianeDionisio <daianedionisio2000@gmail.com>
2023-05-30 01:03:09 -0300

    add caso de falha

--- src/main/java/org/example/ObservadorContagemPalavrasComMaiuscula.java
+++ src/main/java/org/example/ObservadorContagemPalavrasComMaiuscula.java
@@ -1,5 +1,6 @@
 package org.example;
 
+import java.util.EmptyStackException;
 import java.util.List;
 
 public class ObservadorContagemPalavrasComMaiuscula implements iObservador {
@@ -7,13 +8,16 @@ public class ObservadorContagemPalavrasComMaiuscula implements iObservador {
     
     @Override
     public void atualizar(List<String> palavras) {
+        if(palavras.size() == 0) {
+            throw new EmptyStackException();
+        }
         for (String palavra : palavras) {
             if (Character.isUpperCase(palavra.charAt(0))) {
                 contagem++;
             }
         }
     }
-    
+
     public int getContagem() {
         return contagem;
     }
--- src/test/java/testeContagemPalavras.java
+++ src/test/java/testeContagemPalavras.java
@@ -1,8 +1,12 @@
+import org.example.ObservadorContagemPalavrasComMaiuscula;
+import org.example.ObservadorContagemPalavrasComTamanhoPar;
 import org.example.ObservadorContagemTotalPalavras;
 import org.example.Observavel;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
+
+import java.util.EmptyStackException;
 public class testeContagemPalavras {
     @Test
     public void testContarTodasAsPalavras() {
@@ -13,6 +17,38 @@ public class testeContagemPalavras {
         observavel.adicionarObservador(observadorTotal);
         observavel.contarPalavras(frase);
         assertEquals(2, observadorTotal.getContagem());
+    }
+
+    @Test
+    public void testContarPalavrasCaracteresPares() {
+        Observavel observavel = new Observavel();
+        ObservadorContagemPalavrasComTamanhoPar observadorPar = new ObservadorContagemPalavrasComTamanhoPar();
+
+        String frase = "Segundo test";
+        observavel.adicionarObservador(observadorPar);
+        observavel.contarPalavras(frase);
+        assertEquals(1, observadorPar.getContagem());
+    }
+
+    @Test
+    public void testContarPalavrasComMaiuscula() {
+        Observavel observavel = new Observavel();
+        ObservadorContagemPalavrasComMaiuscula observadorMaiuscula = new ObservadorContagemPalavrasComMaiuscula();
+
+        String frase = "Terceiro Teste com Maiuscula";
+        observavel.adicionarObservador(observadorMaiuscula);
+        observavel.contarPalavras(frase);
+        assertEquals(3, observadorMaiuscula.getContagem());
+    }
+
+    @Test(expected = EmptyStackException.class)
+    public void testContarPalavrasComMaiusculaStringVazia() {
+        Observavel observavel = new Observavel();
+        ObservadorContagemPalavrasComMaiuscula observadorMaiuscula = new ObservadorContagemPalavrasComMaiuscula();
+
+        String frase = "";
+        observavel.adicionarObservador(observadorMaiuscula);
+        observavel.contarPalavras(frase);
     }
 
 }
